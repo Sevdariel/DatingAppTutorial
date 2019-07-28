dcbbb204ff10ae977224ec8024a4edcd7eaa4f2c
unknown <zali2008@wp.pl>
2019-07-28 20:24:40 +0200

    Dodanie 3rd party components

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './_components/nav/nav.component';
-
-import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './_components/home/home.component';
 import { RegisterComponent } from './_components/register/register.component';
-import { ErrorInterceptorProvider } from './_services/error.interceptor';
+
+import { ErrorInterceptorProvider } from './_services/error/error.interceptor';
+import { AuthService } from './_services/auth/auth.service';
+import { AlertifyService } from './_services/alertify/alertify.service';
 
 @NgModule({
    declarations: [
@@ -21,11 +23,13 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
    imports: [
       BrowserModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [
       AuthService,
-      ErrorInterceptorProvider
+      ErrorInterceptorProvider,
+      AlertifyService
    ],
    bootstrap: [
       AppComponent
