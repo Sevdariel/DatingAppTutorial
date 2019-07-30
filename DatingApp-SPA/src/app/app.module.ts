71ddc26e510f609a4b5245a185ff5397f344fa70
unknown <zali2008@wp.pl>
2019-07-30 21:33:23 +0200

    Dodanie routingu

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -3,33 +3,45 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './_components/nav/nav.component';
 import { HomeComponent } from './_components/home/home.component';
 import { RegisterComponent } from './_components/register/register.component';
 
+
 import { ErrorInterceptorProvider } from './_services/error/error.interceptor';
 import { AuthService } from './_services/auth/auth.service';
 import { AlertifyService } from './_services/alertify/alertify.service';
+import { appRoutes } from './routes';
+import { ListsComponent } from './_components/lists/lists.component';
+import { MemberListComponent } from './_components/member-list/member-list.component';
+import { MessagesComponent } from './_components/messages/messages.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 @NgModule({
    declarations: [
       AppComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      ListsComponent,
+      MemberListComponent,
+      MessagesComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
-      BsDropdownModule.forRoot()
+      BsDropdownModule.forRoot(),
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [
       AuthService,
       ErrorInterceptorProvider,
-      AlertifyService
+      AlertifyService,
+      AuthGuard
    ],
    bootstrap: [
       AppComponent
