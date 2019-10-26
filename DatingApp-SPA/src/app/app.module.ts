66e85ee4856cb1744076abdbfc7a44590b539188
unknown <zali2008@wp.pl>
2019-10-26 18:43:22 +0200

    Drobne poprawki

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -72,7 +72,7 @@ export function tokenGetter() {
       FileUploadModule,
       JwtModule.forRoot({
          config: {
-            tokenGetter: tokenGetter,
+            tokenGetter,
             whitelistedDomains: ['localhost:5000'],
             blacklistedRoutes: ['localhost:5000/api/auth']
          }
